ee962553b570af5cc26c757ad87de6e9877f266d
Nitheesh Prabu <nitheeshprabu.s@gmail.com>
2019-06-18 16:45:38 +0530

    Added tests for DELETE /todos:id

--- server/server.js
+++ server/server.js
@@ -29,27 +29,41 @@ app.get('/todos', (req, res) => {
 	Todo.find().then((todos) => {
 		return res.send({todos});
 	}).catch((err) => {
-		if (err) {
+		if (err)
 			res.status(400).send(err);
-		}
 	});
 });
 
 app.get('/todos/:id', (req, res) => {
 	var id = req.params.id;
-	if (!ObjectID.isValid(id)) {
+	if (!ObjectID.isValid(id))
 		return res.status(404).send();
-	}
 	
-	Todo.findById(req.params.id).then((todo) => {
-		if (!todo) {
+	Todo.findById(id).then((todo) => {
+		if (!todo)
 			return res.status(404).send();
-		}
+
 		res.send({todo});
 	}).catch((err) => {
 		if (err)
 			res.status(400).send(err);
 	});
+});
+
+app.delete('/todos/:id', (req, res) => {
+	var id = req.params.id;
+	if (!ObjectID.isValid(id))
+		return res.status(404).send();
+
+	Todo.findByIdAndDelete(id).then((todo) => {
+		if (!todo)
+			return res.status(404).send();
+
+		res.send({todo});
+	}).catch((err) => {
+		if (err)
+			res.send(400).send(err);
+	})
 });
 
 app.listen(port, () => {
