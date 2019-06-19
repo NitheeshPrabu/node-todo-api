a80d1d78ac047d769e14875ac3336537288e6659
Nitheesh Prabu <nitheeshprabu.s@gmail.com>
2019-06-19 10:18:06 +0530

    Separated seed data for tests

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,23 +4,11 @@ const {ObjectID} = require('mongodb');
 
 const { app } = require('./../server');
 const { Todo } = require('./../models/todo');
-
-const todos = [{
-	_id: new ObjectID(),
-	text: 'First test todo'
-}, {
-	_id: new ObjectID(),
-	text: 'Second test todo',
-	completed: true,
-	completedAt: 1560857040482
-}]
+const {todos, populateTodos, users, populateUsers } = require('./seed/seed');
 
 // testing lifecycle method
-beforeEach((done) => {
-	Todo.deleteMany({}).then(() => {
-		return Todo.insertMany(todos);
-	}).then(() => done());
-});
+beforeEach(populateUsers);
+beforeEach(populateTodos);
 
 describe('POST /todos', () => {
 	it('should create a new todo', (done) => {
